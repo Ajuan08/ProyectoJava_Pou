2aadb38b04e14547f95cdbb88a53d6e47b49480b
Rios Fernandez, Antonio Juan <93133456+Ajuan08@users.noreply.github.com>
2022-02-03 10:34:55 +0100

    Update App.java

--- src/main/java/riosfernandez/antoniojuan/pou/App.java
+++ src/main/java/riosfernandez/antoniojuan/pou/App.java
@@ -4,6 +4,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,6 +12,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -39,13 +41,20 @@ public class App extends Application {
     int alimento2PositionY =0;
     int alimento3PositionX =350;
     int alimento3PositionY =0;
-    
+    Group pizza = new Group();
+    Group pelota = new Group();
+    Group donut = new Group();
+    Group pou = new Group();
+    Rectangle rectPou;
+    Rectangle rectAlimento;
+    Rectangle rectAlimento2;
+    Rectangle rectAlimento3;
 
     @Override
       public void start(Stage stage) {
           //Crear escena de la pantalla
           Scene scene = new Scene(root, SCENE_TAM_X, SCENE_TAM_Y, Color.BLACK);
-          stage.setTitle("POUS");
+          stage.setTitle("PouS");
           stage.setScene(scene);
           stage.show();
           
@@ -58,20 +67,40 @@ public class App extends Application {
           fondo1.setLayoutY(fondo1PositionY);
           fondo2.setLayoutY(fondo2PositionY);
          
-          //AÑADIENDO LA IMAGEN DEL PERSONAJE
-          Image personaje = new Image(getClass().getResourceAsStream("/images/pou.png"));
+          //AÑADIENDO LA IMAGEN DEL PERSONAJE y Creación del grupo , rectangulo.
+          Image personaje = new Image(getClass().getResourceAsStream("/images/pou.png"));//Creacion de la imagen
           personaje1 = new ImageView(personaje);
-          root.getChildren().add(personaje1);
-          personaje1.setLayoutX(personajePositionX);
-          personaje1.setLayoutY(personajePositionY);
+          rectPou = new Rectangle(150, 500, 80, 80);//Creacion de un rectangulo
+          rectPou.setFill(Color.WHITE);
+          pou.getChildren().addAll(personaje1, rectPou);//Añadimos al grupo el personaje y el rectangulo
+          root.getChildren().add(personaje1);//Añadimos en el root la imagen
+          root.getChildren().add(pou);
+          personaje1.setLayoutX(personajePositionX);//Asignamos la posicionX
+          personaje1.setLayoutY(personajePositionY);//Asignamos la posicionY
           
-          //AÑADIENDO LA IMAGEN DEL ALIMENTO1
+          //AÑADIENDO LA IMAGEN DE LOS ALIMENTOS y Creación del grupo, rectangulo.
+          //Alimento 1
           Image alimento1Img = new Image(getClass().getResourceAsStream("/images/alimento.png"));
           alimento1 = new ImageView(alimento1Img);
+           rectAlimento= new Rectangle(40, 50);
+           rectAlimento.setFill(Color.WHITE);
+           pizza.getChildren().addAll(alimento1, rectAlimento);
+           root.getChildren().add(pizza);
+          //Alimento 2
           Image alimento2Img = new Image(getClass().getResourceAsStream("/images/alimento2.png"));
           alimento2 = new ImageView(alimento2Img);
+          rectAlimento2= new Rectangle(170, 0, 40, 41);
+          rectAlimento2.setFill(Color.WHITE);
+          donut.getChildren().addAll(alimento2, rectAlimento2);
+          root.getChildren().add(donut);
+          //Alimento 3
           Image alimento3Img = new Image(getClass().getResourceAsStream("/images/alimento3.png"));
           alimento3 = new ImageView(alimento3Img);
+          rectAlimento3= new Rectangle(350, 0, 40, 39);
+          rectAlimento3.setFill(Color.WHITE);
+          pelota.getChildren().addAll(alimento3, rectAlimento3);
+          root.getChildren().add(pelota);
+           
           root.getChildren().add(alimento1);
           root.getChildren().add(alimento2);
           root.getChildren().add(alimento3);
@@ -97,11 +126,11 @@ public class App extends Application {
           Timeline scrollAlimento = new Timeline(
                   new KeyFrame(Duration.seconds(0.017), (ActionEvent ae) -> {
                       alimento1PositionY = alimento1PositionY +2;
-                      alimento1.setLayoutY(alimento1PositionY);
+                      pizza.setLayoutY(alimento1PositionY);
                       alimento2PositionY = alimento2PositionY +2;
-                      alimento2.setLayoutY(alimento2PositionY);
+                      donut.setLayoutY(alimento2PositionY);
                       alimento3PositionY = alimento3PositionY +2;
-                      alimento3.setLayoutY(alimento3PositionY);
+                      pelota.setLayoutY(alimento3PositionY);
                       if(alimento1PositionY >= 600){
                           alimento1PositionY = 0;
                       }else if(alimento2PositionY >= 600){
@@ -130,7 +159,10 @@ public class App extends Application {
         });
           //Colisionado de los objetos
           
-            
+          
+          
+          // Parte del código para detectar la colisión de los rectángulos
+            //Shape zonaColision = Shape.intersect(rectPou, rectZona2);
           scrollFondo.setCycleCount(Timeline.INDEFINITE);
           scrollFondo.play();
           scrollAlimento.setCycleCount(Timeline.INDEFINITE);
